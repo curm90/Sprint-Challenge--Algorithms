efbc30c685646d53ea92946d51c29506cc2d1608
Liam Sutton <devliam90@gmail.com>
2020-02-21 08:47:26 +0000

    recursuve count solution

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -7,10 +7,15 @@ Your function must utilize recursion. It cannot contain any loops.
 
 def count_th(word):
     # a place to store the count
+    count = 0
     # if len word < 1 return 0
+    if len(word) < 1:
+        return 0
     # else
-    # slice arr with first 2 indeces and check if if == to 'th'
-    # if yes increment count
+    else:
+        # slice arr with first 2 indeces and check if if == to 'th'
+        if word[:2] == 'th':
+            # if yes increment count
+            count += 1
     # return count + count_th with next arr
-
-    pass
+    return count + count_th(word[1:])
